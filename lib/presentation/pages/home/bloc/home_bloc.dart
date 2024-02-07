2a8826ec3edab30c262d6eaf102821d778bc4f33
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-08 00:27:40 +0530

    Add- Home Bloc

--- lib/presentation/pages/home/bloc/home_bloc.dart
+++ lib/presentation/pages/home/bloc/home_bloc.dart
@@ -1,13 +1,23 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'dart:async';
 
+import 'package:bloc/bloc.dart';
+import 'package:fluxestore/models/product_data_model.dart';
+import 'package:fluxestore/repository/products_repository.dart';
+import 'package:meta/meta.dart';
 part 'home_event.dart';
 part 'home_state.dart';
 
-class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class HomePageBloc extends Bloc<HomePageEvent, HomePageState> {
+  HomePageBloc() : super(HomeInitial()) {
+    on<HomePageInitialEvent>(homePageInitialEvent);
+  }
+
+  FutureOr<void> homePageInitialEvent(
+      HomePageInitialEvent event, Emitter<HomePageState> emit) async {
+    emit(HomePageLoadingState());
+
+    var data = await ProductsRepository().getAllProducts();
+
+    emit(HomePageLoadedSuccessState(loadedProductsList: data));
   }
 }
