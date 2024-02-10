defb31865c6e54ee0a74725f40235cf48ee2bc36
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-11 00:55:17 +0530

    Add - endpoints for login and signup

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -1,6 +1,10 @@
 class Api {
   String baseUrl = "http://192.168.1.5:5000";
 
+  String loginUrl = "http://192.168.1.5:5000/api/v1/user/login";
+
+  String registerUrl = "http://192.168.1.5:5000/api/v1/user/registerUser";
+
   String productsUrl = "http://192.168.1.5:5000/products";
 
   String bannersUrl = "http://192.168.1.5:5000/api/v1/getAllBanners";
