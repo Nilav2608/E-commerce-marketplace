56ef7d6a8cf390122292eb9162ac16dd81e749bb
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-10 14:46:57 +0530

    Add- banners api

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -2,4 +2,6 @@ class Api {
   String baseUrl = "http://192.168.1.5:5000";
 
   String productsUrl = "http://192.168.1.5:5000/products";
+
+  String bannersUrl = "http://192.168.1.5:5000/api/v1/getAllBanners";
 }
