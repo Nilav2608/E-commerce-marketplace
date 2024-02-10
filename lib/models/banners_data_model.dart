ea2dc6f5e33886751e40b87d5e4d19f4a0022345
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-10 14:45:16 +0530

    Fix - BannersDataModel text-> title

--- lib/models/banners_data_model.dart
+++ lib/models/banners_data_model.dart
@@ -1,27 +1,31 @@
 class BannersDataModel {
-  final int id;
-  final String text;
+  final String id;
+  final String title;
   final String imagePath;
+  final String category;
 
   BannersDataModel({
     required this.id,
-    required this.text,
+    required this.title,
     required this.imagePath,
+    required this.category,
   });
 
   factory BannersDataModel.fromJson(Map<String, dynamic> json) {
     return BannersDataModel(
-      id: json['id'] as int,
-      text: json['text'] as String,
+      id: json['_id'] as String,
+      title: json['title'] as String,
       imagePath: json['imagePath'] as String,
+      category: json['category'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'text': text,
+      '_id': id,
+      'title': title,
       'imagePath': imagePath,
+      'category': category,
     };
   }
 }
