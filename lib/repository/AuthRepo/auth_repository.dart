91af2a93625a58903da452639c5a12818d87f314
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-11 19:27:18 +0530

    New Auth Repository

---
+++ lib/repository/AuthRepo/auth_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:fluxestore/network/api.dart';
+import 'package:fluxestore/repository/AuthRepo/auth_repository.interface.dart';
+
+class AuthRepository extends Api implements IAuthRepository {
+  @override
+  Future<Map<String, dynamic>> login(String email, String password) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Map<String, dynamic>> register(
+      String userName, String email, String password) async {
+    try {
+      var details = {
+        "userName": userName,
+        "email": email,
+        "password": password
+      };
+      var response = await http.post(Uri.parse(registerUrl),
+          headers: {"content-type": "application/json"},
+          body: jsonEncode(details));
+      var results = jsonDecode(response.body);
+      if (response.statusCode == 201) {
+        return results;
+      } else if (response.statusCode == 400) {
+        return results;
+      } else {
+        return results;
+      }
+    } catch (e) {
+      return {"status": false, "message": "An Internal error accurd"};
+    }
+  }
+}
