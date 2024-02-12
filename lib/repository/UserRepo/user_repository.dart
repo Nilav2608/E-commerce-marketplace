0fa0599ba76f7f5e9e3fc874d8b692f8a5fe1a21
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-12 23:29:47 +0530

    Add -get user data repo impl

---
+++ lib/repository/UserRepo/user_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:fluxestore/models/user_data_model.dart';
+import 'package:fluxestore/repository/UserRepo/user_repository.interface.dart';
+
+import '../../network/api.dart';
+
+class USerRepository extends Api implements IUserRepository {
+  @override
+  Future<Map<String, dynamic>> getUserDetails(String userId) async {
+    try {
+      print("Repo invoked");
+      print(userId);
+
+      var response = await http.post(Uri.parse(userUrl),
+          headers: {"content-type": "application/json"},
+          body: jsonEncode({"userId": userId}));
+      var results = jsonDecode(response.body);
+      if (response.statusCode == 201) {
+        return results;
+      } else if (response.statusCode == 401) {
+        return results;
+      } else if (response.statusCode == 400) {
+        return results;
+      } else {
+        return results;
+      }
+    } catch (e) {
+      return {"status": false, "message": "An Internal error accurd"};
+    }
+  }
+}
