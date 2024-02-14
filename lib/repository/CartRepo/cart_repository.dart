e03c740f3f56a852cbbac1c663987b5f009e259e
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-15 01:05:41 +0530

    Add - get and delete cart items from server

--- lib/repository/CartRepo/cart_repository.dart
+++ lib/repository/CartRepo/cart_repository.dart
@@ -15,11 +15,54 @@ class CartRepository extends Api implements ICartRepository {
           headers: {"content-type": "application/json"},
           body: jsonEncode(cartItem));
       var results = jsonDecode(response.body);
-      print(results);
       if (response.statusCode == 201) {
         return results;
       } else {
         throw Exception("failed to get data");
+      }
+    } catch (e) {
+      return {"status": false, "message": e};
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> getCartItems(String userId) async {
+    try {
+      var response = await http.post(Uri.parse(getUserCartUrl),
+          headers: {"content-type": "application/json"},
+          body: jsonEncode({'userId': userId}));
+      var results = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        return results;
+      } else if (response.statusCode == 400) {
+        return results;
+      } else {
+        throw Exception("failed to get data");
+      }
+    } catch (e) {
+      return {"status": false, "message": e};
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> deleteCartItem(
+      String userId, String docId) async {
+    try {
+
+      var quaries = {'docId': docId, 'userId': userId};
+
+      var response = await http.delete(Uri.parse(deleteUserCart),
+          headers: {"content-type": "application/json"},
+          body: jsonEncode(quaries));
+
+      var results = jsonDecode(response.body);
+      
+      if (response.statusCode == 200) {
+        return results;
+      } else if (response.statusCode == 400) {
+        return results;
+      } else {
+        return results;
       }
     } catch (e) {
       return {"status": false, "message": e};
