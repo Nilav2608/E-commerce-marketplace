89f4c03c6f164c9c21dcfdafd14a6425f18e2aa6
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-16 00:56:22 +0530

    "

--- lib/presentation/pages/checkout/checkout_main.view.dart
+++ lib/presentation/pages/checkout/checkout_main.view.dart
@@ -46,207 +46,210 @@ class _CheckOutPageState extends State<CheckOutPage> {
                 fontSize: 20,
                 fontWeight: FontWeight.w700)),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Builder(builder: (context) {
-              return BlocConsumer<CheckOutPageBloc, CheckOutPageState>(
-                listenWhen: (previous, current) =>
-                    current is CheckOutPageActionState,
-                buildWhen: (previous, current) =>
-                    current is CheckOutPageActionState,
-                bloc: checkOutPageBloc,
-                listener: (context, state) {
-                  switch (state.runtimeType) {
-                    case NavigateTohomePageActionState:
-                      Navigator.popAndPushNamed(context, '/');
-                  }
-                },
-                builder: (context, state) {
-                  switch (state.runtimeType) {
-                    case PageLoadingState:
-                      return const AlertDialog(
-                        content: CircularProgressIndicator(),
-                      );
-
-                    case CheckOutPageBlocInitialState:
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const ChekoutStatusItemsView(
-                            initState: true,
-                            shippingAddressState: false,
-                            paymentState: false,
-                            completedState: false,
-                          ),
-                          ShippingAddressSection(
-                            onPressed: (DeliveryAddress deliveryAddress) {
-                              if (formKey.currentState!.validate()) {
-                                checkOutPageBloc.add(CheckOutPagePaymentEvent(
-                                    subTotal: widget.myOrdersData.subTotal!,
-                                    addressData: deliveryAddress));
-                                // ScaffoldMessenger.of(context).showSnackBar(
-                                //     const SnackBar(
-                                //         content: Text("Submitting form")));
-                              }
-                            },
-                          )
-                        ],
-                      );
-                    case PaymentPageActionState:
-                      final successData = state as PaymentPageActionState;
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const ChekoutStatusItemsView(
-                            initState: true,
-                            shippingAddressState: true,
-                            paymentState: true,
-                            completedState: false,
-                          ),
-                          PaymentSectionView(
-                            subTotal: successData.subTotal,
-                            onChanged: (agreedToTermsAndConditions) {
-                              onChanged(agreedToTermsAndConditions);
-                            },
-                            agreedToTermsAndConditions:
-                                agreedToTermsAndConditions,
-                            onProceedToCheckOut: (paymentMethod) {
-                              if (agreedToTermsAndConditions) {
-                                checkOutPageBloc.add(
-                                    CheckOutPagePlaceOrderEvent(
-                                        dataModel: widget.myOrdersData,
-                                        addressData: successData.address,
-                                        paymentMode: paymentMethod));
-                              } else {
-                                ScaffoldMessenger.of(context)
-                                    .showSnackBar(const SnackBar(
-                                        behavior: SnackBarBehavior.floating,
-                                        backgroundColor: Color(0xFFFF8080),
-                                        content: Text(
-                                          "Please verify terms and conditions to proceed!",
-                                          style: TextStyle(color: Colors.white),
-                                        )));
-                              }
-                            },
-                          )
-                          // Positioned(
-                          //   left: 0,
-                          //   right: 0,
-                          //   bottom: 0,
-                          //   height: 80, // Height of the bottom container
-                          //   child: Container(
-                          //     height: 300,
-                          //     color: Colors.blue, // Example background color
-                          //     child: Column(
-                          //       mainAxisAlignment:
-                          //           MainAxisAlignment.spaceAround,
-                          //       children: [
-                          //         IconButton(
-                          //           icon: Icon(Icons.home),
-                          //           onPressed: () {
-                          //             // Handle button tap
-                          //           },
-                          //         ),
-                          //         IconButton(
-                          //           icon: Icon(Icons.settings),
-                          //           onPressed: () {
-                          //             // Handle button tap
-                          //           },
-                          //         ),
-                          //         IconButton(
-                          //           icon: Icon(Icons.person),
-                          //           onPressed: () {
-                          //             // Handle button tap
-                          //           },
-                          //         ),
-                          //       ],
-                          //     ),
-                          //   ),
-                          // ),
-                        ],
-                      );
-                    case PaymentCompletedActionState:
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const ChekoutStatusItemsView(
-                            initState: true,
-                            shippingAddressState: true,
-                            paymentState: true,
-                            completedState: true,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text("Order Completed",
-                                    style: TextStyle(
-                                        color: Color(0xff1D1F22),
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w700)),
-                                const SizedBox(
-                                  height: 50,
-                                ),
-                                Center(
-                                  child: Lottie.asset(
-                                      "assets/lottie/completed_lottie.json",
-                                      width: 200,
-                                      height: 200,
-                                      repeat: false),
-                                ),
-                                const Center(
-                                  child: SizedBox(
-                                    height: 75,
-                                    width: 272,
-                                    child: Text(
-                                        "Thank you for your purchase!, You can view your order in ‘My Orders’ section.",
-                                        style: TextStyle(
-                                            color: Color(0xff1D1F22),
-                                            fontSize: 14,
-                                            letterSpacing: 1,
-                                            fontWeight: FontWeight.w400)),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 50,
-                                ),
-                                SizedBox(
-                                  height: 48,
-                                  width: double.infinity,
-                                  child: ElevatedButton(
-                                      onPressed: () {
-                                        checkOutPageBloc
-                                            .add(NavigateBackToHomePageEvent());
-                                      },
-                                      style: const ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStatePropertyAll(
-                                                  Color(0xFF343434))),
-                                      child: const Text(
-                                        "Continue shopping",
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w700,
-                                            color: Colors.white),
-                                      )),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      );
-
-                    default:
-                      return const SizedBox();
-                  }
-                },
+      body: BlocConsumer<CheckOutPageBloc, CheckOutPageState>(
+        listenWhen: (previous, current) =>
+            current is CheckOutPageActionState,
+        buildWhen: (previous, current) =>
+            current is! CheckOutPageActionState,
+        bloc: checkOutPageBloc,
+        listener: (context, state) {
+          switch (state.runtimeType) {
+            case NavigateTohomePageActionState:
+              Navigator.popAndPushNamed(context, '/');
+            
+            case PageLoadingState:
+              AlertDialog(
+                backgroundColor: Colors.lightBlue[100],
+                content: const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircularProgressIndicator(),
+                    SizedBox(height: 16),
+                    Text(
+                      'Placing your order...',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
               );
-            }),
-          ),
-        ],
+          }
+        },
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case CheckOutPageBlocInitialState:
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ChekoutStatusItemsView(
+                    initState: true,
+                    shippingAddressState: false,
+                    paymentState: false,
+                    completedState: false,
+                  ),
+                  ShippingAddressSection(
+                    onPressed: (DeliveryAddress deliveryAddress) {
+                      if (formKey.currentState!.validate()) {
+                        checkOutPageBloc.add(CheckOutPagePaymentEvent(
+                            subTotal: widget.myOrdersData.subTotal!,
+                            addressData: deliveryAddress));
+                        // ScaffoldMessenger.of(context).showSnackBar(
+                        //     const SnackBar(
+                        //         content: Text("Submitting form")));
+                      }
+                    },
+                  )
+                ],
+              );
+            case PaymentPageActionState:
+              final successData = state as PaymentPageActionState;
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ChekoutStatusItemsView(
+                    initState: true,
+                    shippingAddressState: true,
+                    paymentState: true,
+                    completedState: false,
+                  ),
+                  PaymentSectionView(
+                    subTotal: successData.subTotal,
+                    onChanged: (agreedToTermsAndConditions) {
+                      onChanged(agreedToTermsAndConditions);
+                    },
+                    agreedToTermsAndConditions:
+                        agreedToTermsAndConditions,
+                    onProceedToCheckOut: (paymentMethod) {
+                      if (agreedToTermsAndConditions) {
+                        checkOutPageBloc.add(
+                            CheckOutPagePlaceOrderEvent(
+                                dataModel: widget.myOrdersData,
+                                addressData: successData.address,
+                                paymentMode: paymentMethod));
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                                behavior: SnackBarBehavior.floating,
+                                backgroundColor: Color(0xFFFF8080),
+                                content: Text(
+                                  "Please verify terms and conditions to proceed!",
+                                  style: TextStyle(color: Colors.white),
+                                )));
+                      }
+                    },
+                  )
+                  // Positioned(
+                  //   left: 0,
+                  //   right: 0,
+                  //   bottom: 0,
+                  //   height: 80, // Height of the bottom container
+                  //   child: Container(
+                  //     height: 300,
+                  //     color: Colors.blue, // Example background color
+                  //     child: Column(
+                  //       mainAxisAlignment:
+                  //           MainAxisAlignment.spaceAround,
+                  //       children: [
+                  //         IconButton(
+                  //           icon: Icon(Icons.home),
+                  //           onPressed: () {
+                  //             // Handle button tap
+                  //           },
+                  //         ),
+                  //         IconButton(
+                  //           icon: Icon(Icons.settings),
+                  //           onPressed: () {
+                  //             // Handle button tap
+                  //           },
+                  //         ),
+                  //         IconButton(
+                  //           icon: Icon(Icons.person),
+                  //           onPressed: () {
+                  //             // Handle button tap
+                  //           },
+                  //         ),
+                  //       ],
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              );
+            case PaymentCompletedState:
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ChekoutStatusItemsView(
+                    initState: true,
+                    shippingAddressState: true,
+                    paymentState: true,
+                    completedState: true,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Order Completed",
+                            style: TextStyle(
+                                color: Color(0xff1D1F22),
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700)),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Center(
+                          child: Lottie.asset(
+                              "assets/lottie/completed_lottie.json",
+                              width: 200,
+                              height: 200,
+                              repeat: false),
+                        ),
+                        const Center(
+                          child: SizedBox(
+                            height: 75,
+                            width: 272,
+                            child: Text(
+                                "Thank you for your purchase!, You can view your order in ‘My Orders’ section.",
+                                style: TextStyle(
+                                    color: Color(0xff1D1F22),
+                                    fontSize: 14,
+                                    letterSpacing: 1,
+                                    fontWeight: FontWeight.w400)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 48,
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                checkOutPageBloc
+                                    .add(NavigateBackToHomePageEvent());
+                              },
+                              style: const ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(
+                                          Color(0xFF343434))),
+                              child: const Text(
+                                "Continue shopping",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white),
+                              )),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              );
+            
+            default:
+              return const SizedBox();
+          }
+        },
       ),
     );
   }
