e7d027da671c528ce8bcf67162c5d28768283c6b
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-20 21:08:56 +0530

    New animation init

--- lib/presentation/pages/MyOrders/my_orders_list.dart
+++ lib/presentation/pages/MyOrders/my_orders_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluxestore/models/my_orders_data_model.dart';
 import 'package:fluxestore/presentation/pages/MyOrders/my_order_details_card.dart';
-
+import 'package:lottie/lottie.dart';
 
 class OrdersList extends StatelessWidget {
   final List<MyOrdersDataModel> items;
@@ -9,24 +9,36 @@ class OrdersList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Expanded(
       child: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                var data = items[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: MyOrderDetailsCard(
-                    data: data,
+          items.isEmpty
+              ? SizedBox(
+                  // width: screenWidth*0.100,
+                  // height: screenHeight*0.100,
+                  child: Lottie.asset(
+                    "assets/lottie/empty_order.json",
+                    width: screenWidth * 0.50,
+                    height: screenHeight * 0.50,
                   ),
-                );
-              },
-            ),
-          )
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      var data = items[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: MyOrderDetailsCard(
+                          data: data,
+                        ),
+                      );
+                    },
+                  ),
+                )
         ],
       ),
     );
