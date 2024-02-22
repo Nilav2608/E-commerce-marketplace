8d9672ca546835d206d0cbfa8fc290cf63289f1d
Nilavarasu2608 <knlsu444@gmail.com>
2024-02-22 15:17:42 +0530

    id property removed

--- lib/data/product_recomendation_data.dart
+++ lib/data/product_recomendation_data.dart
@@ -2,40 +2,31 @@ import '../models/product_data_model.dart';
 
 List<ProductDataModel> recomendationsList = [
   ProductDataModel(
-      id: "3",
       productName: "Rise Crop Hoodie",
       descriptionn: "xxxxxxx",
       price: 43.00,
       imageUrl:
           "https://t3.gstatic.com/images?q=tbn:ANd9GcS_zRNrc2kC5Rg-OiFLvyRKUy-A3jWuzSjvQEluYLkZ7JK_m-wd",
-      //  imageUrl: "https://firebasestorage.googleapis.com/v0/b/fluxestore-b6cb3.appspot.com/o/StreetWear.png?alt=media&token=f40e7bb5-f8b7-4f06-a8e1-1a32da385eb9",
-      // imageUrl: "assets/images/StreetWear.svg",
       sizes: ["S", "M", "L", "XL"],
       colors: ["Orange,Black,Pink"],
       favorite: false,
       ratings: 4),
   ProductDataModel(
-      id: "3",
       productName: "Gym Crop Top",
       descriptionn: "xxxxxxx",
       price: 43.00,
       imageUrl:
           "https://t0.gstatic.com/images?q=tbn:ANd9GcSAUghSQTP_7jtcWnvcO79jzGuJJ-S9Kj98OGyjvBGzm_AVGXyq",
-      //  imageUrl: "https://firebasestorage.googleapis.com/v0/b/fluxestore-b6cb3.appspot.com/o/StreetWear.png?alt=media&token=f40e7bb5-f8b7-4f06-a8e1-1a32da385eb9",
-      // imageUrl: "assets/images/StreetWear.svg",
       sizes: ["S", "M", "L", "XL"],
       colors: ["Orange,Black,Pink"],
       favorite: false,
       ratings: 4),
   ProductDataModel(
-      id: "3",
       productName: "Sport Sweatshirt",
       descriptionn: "xxxxxxx",
       price: 43.00,
       imageUrl:
           "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/1/2/tr:w-480,/123205cptsblu_1_cdd396fd.jpg?rnd=20200526195200",
-      //  imageUrl: "https://firebasestorage.googleapis.com/v0/b/fluxestore-b6cb3.appspot.com/o/StreetWear.png?alt=media&token=f40e7bb5-f8b7-4f06-a8e1-1a32da385eb9",
-      // imageUrl: "assets/images/StreetWear.svg",
       sizes: ["S", "M", "L", "XL"],
       colors: ["Orange,Black,Pink"],
       favorite: false,
