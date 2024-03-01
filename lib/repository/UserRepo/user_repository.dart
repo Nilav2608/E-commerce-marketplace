724ec8283b15390fc64bee5e3f822398aef955c9
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-01 23:44:30 +0530

    Refactor

--- lib/repository/UserRepo/user_repository.dart
+++ lib/repository/UserRepo/user_repository.dart
@@ -4,29 +4,19 @@ import 'package:http/http.dart' as http;
 import 'package:fluxestore/repository/UserRepo/user_repository.interface.dart';
 
 import '../../network/Api/api.dart';
+import '../../network/Exceptions/app_exceptions.dart';
+import '../../network/Exceptions/exception_handlers.dart';
 
 class USerRepository extends Api implements IUserRepository {
   @override
   Future<Map<String, dynamic>> getUserDetails(String userId) async {
     try {
-      print("Repo invoked");
-      print(userId);
-
       var response = await http.post(Uri.parse(userUrl),
           headers: {"content-type": "application/json"},
           body: jsonEncode({"userId": userId}));
-      var results = jsonDecode(response.body);
-      if (response.statusCode == 201) {
-        return results;
-      } else if (response.statusCode == 401) {
-        return results;
-      } else if (response.statusCode == 400) {
-        return results;
-      } else {
-        return results;
-      }
+      return processResponse(response);
     } catch (e) {
-      return {"status": false, "message": "An Internal error accurd"};
+      return ExceptionHandlers().getExceptionString(e);
     }
   }
 }
