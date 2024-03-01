4916b559e592a4396804abc95c06078b99e0165a
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-01 23:50:58 +0530

    Refactored with custom response and exception handlers

--- lib/repository/CartRepo/cart_repository.dart
+++ lib/repository/CartRepo/cart_repository.dart
@@ -4,24 +4,20 @@ import 'package:fluxestore/models/cart_items_model.dart';
 import 'package:fluxestore/repository/CartRepo/cart_repository.interface.dart';
 import 'package:http/http.dart' as http;
 import '../../network/Api/api.dart';
+import '../../network/Exceptions/app_exceptions.dart';
+import '../../network/Exceptions/exception_handlers.dart';
 
 class CartRepository extends Api implements ICartRepository {
   @override
   Future<Map<String, dynamic>> addTocart(CartItemsModel data) async {
     try {
       var cartItem = data.toJson();
-      print(cartItem);
       var response = await http.post(Uri.parse(addToCartUrl),
           headers: {"content-type": "application/json"},
           body: jsonEncode(cartItem));
-      var results = jsonDecode(response.body);
-      if (response.statusCode == 201) {
-        return results;
-      } else {
-        throw Exception("failed to get data");
-      }
+      return processResponse(response);
     } catch (e) {
-      return {"status": false, "message": e};
+      return ExceptionHandlers().getExceptionString(e);
     }
   }
 
@@ -31,16 +27,9 @@ class CartRepository extends Api implements ICartRepository {
       var response = await http.post(Uri.parse(getUserCartUrl),
           headers: {"content-type": "application/json"},
           body: jsonEncode({'userId': userId}));
-      var results = jsonDecode(response.body);
-      if (response.statusCode == 200) {
-        return results;
-      } else if (response.statusCode == 400) {
-        return results;
-      } else {
-        throw Exception("failed to get data");
-      }
+      return processResponse(response);
     } catch (e) {
-      return {"status": false, "message": e};
+      return ExceptionHandlers().getExceptionString(e);
     }
   }
 
@@ -48,24 +37,15 @@ class CartRepository extends Api implements ICartRepository {
   Future<Map<String, dynamic>> deleteCartItem(
       String userId, String docId) async {
     try {
-
       var quaries = {'docId': docId, 'userId': userId};
 
       var response = await http.delete(Uri.parse(deleteUserCartUrl),
           headers: {"content-type": "application/json"},
           body: jsonEncode(quaries));
 
-      var results = jsonDecode(response.body);
-      
-      if (response.statusCode == 200) {
-        return results;
-      } else if (response.statusCode == 400) {
-        return results;
-      } else {
-        return results;
-      }
+      return processResponse(response);
     } catch (e) {
-      return {"status": false, "message": e};
+      return ExceptionHandlers().getExceptionString(e);
     }
   }
 }
