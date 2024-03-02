90678f4a02f0366e6de90bf44a5510799bc34974
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-02 23:53:21 +0530

    New page init - productsPage

--- lib/presentation/reuseables/row_heading_seemore.dart
+++ lib/presentation/reuseables/row_heading_seemore.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 class RowHeadsSeeMore extends StatelessWidget {
   final String text1;
   final String text2;
-  const RowHeadsSeeMore({super.key, required this.text1, required this.text2});
+  final Function()? onTapseeMore;
+  const RowHeadsSeeMore({
+    super.key,
+    required this.text1,
+    required this.text2,
+    this.onTapseeMore,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +21,13 @@ class RowHeadsSeeMore extends StatelessWidget {
           style: const TextStyle(
               fontSize: 18, fontWeight: FontWeight.w700, color: Colors.black),
         ),
-        Text(
-          text2,
-          style: const TextStyle(
-              fontSize: 13, fontWeight: FontWeight.w400, color: Colors.grey),
+        InkWell(
+          onTap: onTapseeMore,
+          child: Text(
+            text2,
+            style: const TextStyle(
+                fontSize: 13, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
         ),
       ],
     );
