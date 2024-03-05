6e77117a7752b4c1e3ac72ca33f9e74a80052cc7
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-06 00:01:56 +0530

    Fixed doule value overloaded error

--- lib/utils/Mappers/mappers.dart
+++ lib/utils/Mappers/mappers.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:fluxestore/constants/constants.dart';
 
 import '../../models/cart_items_model.dart';
@@ -7,22 +5,23 @@ import '../../models/delivery_address_model.dart';
 import '../../models/my_orders_data_model.dart';
 import '../../models/product_data_model.dart';
 
-class Mappers{
-  CartItemsModel mapProductsFromProductDataModel(ProductDataModel data,int colorIndex, int sizeIndex) {
-  return CartItemsModel(
-      userId: email,
-      productId: data.id.toString(),
-      productName: data.productName,
-      price: data.price,
-      imageUrl: data.imageUrl,
-      color: data.colors![colorIndex],
-      size: data.sizes![sizeIndex].toString(),
-      quantity: 1,
-      selected: true);
-}
+class Mappers {
+  CartItemsModel mapProductsFromProductDataModel(
+      ProductDataModel data, int colorIndex, int sizeIndex) {
+    return CartItemsModel(
+        userId: email,
+        productId: data.id.toString(),
+        productName: data.productName,
+        price: data.price,
+        imageUrl: data.imageUrl,
+        color: data.colors![colorIndex],
+        size: data.sizes![sizeIndex].toString(),
+        quantity: 1,
+        selected: true);
+  }
 
-   MyOrdersDataModel myOrdersDataMapper(
-      List<CartItemsModel> cartItems, double subTotal,String userId) {
+  MyOrdersDataModel myOrdersDataMapper(
+      List<CartItemsModel> cartItems, double subTotal, String userId) {
     List<CartItemsModel> selectedItems = [];
     int totalQuantity = 0;
     for (int i = 0; i < cartItems.length; i++) {
@@ -33,6 +32,8 @@ class Mappers{
     for (var i = 0; i < selectedItems.length; i++) {
       totalQuantity += selectedItems[i].quantity!;
     }
+    var tempSubTotal = subTotal.toStringAsFixed(2);
+    double convertedSubTotal = double.parse(tempSubTotal);
     return MyOrdersDataModel(
         date: DateTime.now().toString(),
         deliveryAddress: DeliveryAddress(),
@@ -42,8 +43,8 @@ class Mappers{
         orderedItems: selectedItems,
         quantity: totalQuantity,
         shippingCharges: 0.00,
-        subTotal: subTotal,
-        total: subTotal,
+        subTotal: convertedSubTotal,
+        total: convertedSubTotal,
         trackingNumber: "IK987362341");
   }
 }
