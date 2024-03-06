e797de5dd72f04b52a3b1cffb963e84acd3adfe5
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-06 22:28:36 +0530

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        //  textTheme:  _buildTextTheme(ThemeData.light().textTheme),
         fontFamily: "ProductSans",
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       initialRoute:
