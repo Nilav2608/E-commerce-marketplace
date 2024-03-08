bbac4d5dbd7dfd07499beacc534071f9cb64723e
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-08 14:03:39 +0530

    environment variables init

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,24 @@ import 'package:fluxestore/presentation/landing_page/landing_page.dart';
 import 'package:fluxestore/routes/routes.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 int? initScreen;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
-
   initScreen = prefs.getInt("initScreen");
   await prefs.setInt("initScreen", 1);
-
-  runApp(MyApp(token: prefs.getString("token"),initScreen: initScreen,));
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp(
+    token: prefs.getString("token"),
+    initScreen: initScreen,
+  ));
 }
 
 class MyApp extends StatelessWidget {
   final String? token;
-    final int? initScreen;
+  final int? initScreen;
   const MyApp({super.key, required this.token, required this.initScreen});
 
   @override
@@ -31,7 +34,9 @@ class MyApp extends StatelessWidget {
       initialRoute:
           (token != null && JwtDecoder.isExpired(token.toString()) == false)
               ? '/'
-              : (initScreen == 0 || initScreen == null) ? "onBording" : 'Authentication',
+              : (initScreen == 0 || initScreen == null)
+                  ? "onBording"
+                  : 'Authentication',
       routes: {'/': (context) => LandingPage(token: token ?? '')},
       onGenerateRoute: MyGenerateRoute().generateRoute,
     );
