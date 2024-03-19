b6f2b30934d3709d205ff8a56264289973abc82b
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-20 00:48:19 +0530

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluxestore/presentation/landing_page/landing_page.dart';
-import 'package:fluxestore/routes/routes.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:fluxestore/application.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -13,33 +11,8 @@ Future<void> main() async {
   //To show the onbording screen only at the first installation
   await prefs.setInt("initScreen", 1);
   await dotenv.load(fileName: '.env');
-  runApp(MyApp(
+  runApp(Application(
     token: prefs.getString("token"),
     initScreen: initScreen,
   ));
-}
-
-class MyApp extends StatelessWidget {
-  final String? token;
-  final int? initScreen;
-  const MyApp({super.key, required this.token, required this.initScreen});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "ProductSans",
-        useMaterial3: true,
-      ),
-      initialRoute:
-          (token != null && JwtDecoder.isExpired(token.toString()) == false)
-              ? '/'
-              : (initScreen == 0 || initScreen == null)
-                  ? "onBording"
-                  : 'Authentication',
-      routes: {'/': (context) => LandingPage(token: token ?? '')},
-      onGenerateRoute: MyGenerateRoute().generateRoute,
-    );
-  }
 }
