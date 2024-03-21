a57c688cd643f679b751d09f29845571db8658f9
Nilavarasu2608 <knlsu444@gmail.com>
2024-03-21 14:53:19 +0530

    Refactor

--- lib/data/collections_data.dart
+++ lib/data/collections_data.dart
@@ -9,8 +9,6 @@ final List<ProductDataModel> seasonCollectionsData = [
       price: 85.00,
       imageUrl:
           "https://www.na-kd.com/resize/globalassets/nakd_cable_knitted_vest_dress_1018-007408-0342_01j.jpg?ref=8E00AABB7E&quality=80&sharpen=0.3&width=540",
-      //  imageUrl: "https://firebasestorage.googleapis.com/v0/b/fluxestore-b6cb3.appspot.com/o/StreetWear.png?alt=media&token=f40e7bb5-f8b7-4f06-a8e1-1a32da385eb9",
-      // imageUrl: "assets/images/StreetWear.svg",
       sizes: ["S", "M", "L", "XL"],
       colors: ["Peach","Brown","Pink"],
       favorite: false,
@@ -33,7 +31,6 @@ final List<ProductDataModel> seasonCollectionsData = [
       price: 25.00,
       imageUrl:
           "https://rukminim2.flixcart.com/image/832/832/kupuljk0/top/7/k/n/m-round-neck-t-shirt-short-sleeves-redamancii-original-imag7s3hfdkazymp.jpeg?q=70",
-      // imageUrl: "assets/images/StreetWear.svg",
       sizes: ["S", "M", "L", "XL"],
       colors: ["Peach","Brown","Pink"],
       favorite: false,
